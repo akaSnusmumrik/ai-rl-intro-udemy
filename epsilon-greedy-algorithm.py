95602788f7c59bb852e2d6c934f8a632d9fc3b6f
akaSnusmumrik <samsonov.vl.vl@gmail.com>
2018-07-08 19:35:16 +0200

    Third commit

--- epsilon-greedy-algorithm.py
+++ epsilon-greedy-algorithm.py
@@ -1,25 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
-N = 50
-x = np.random.rand(N)
-y = np.random.rand(N)
-colors = np.random.rand(N)
-area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radii
-plt.scatter(x, y, s=area, c=colors, alpha=0.5)
-plt.show()
+import random
+#nitter, epsilon, prob_lst_a=[0.5, 0.5],
+#                        prob_lst_b=[0.5, 0.5],
+#                        prob_lst_c=[0.5, 0.5]
+class epsilon_greedy_rl(object):
 
-X = np.linspace(-np.pi, np.pi, 256,endpoint=True)
-C,S = np.cos(X), np.sin(X)
+    def __init__(self, epsilon, win_probs):
+        self.means = [0, 0, 0]
+        self.epsilon = epsilon
+        self.win_prob_lst = win_probs
 
-plt.plot(X, C, color="blue", linewidth=2.5, linestyle="-")
-plt.plot(X, S, color="red", linewidth=2.5, linestyle="-")
+    def exploreORexploit(self):
+        throw_coin = np.random.uniform(0, 1)
+        if throw_coin < self.epsilon: #exploite
+            return 'explore'
+        else:
+            return 'exploit'
 
-plt.xlim(X.min()*1.1, X.max()*1.1)
-plt.xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
-[r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+    def play_machine(self, machine_ind):
+        # throw_coin = random.uniform(0, 1)
+        # if throw_coin < self.win_prob[machine_ind]:
+        #     return 1
+        # else:
+        #     return 0
+        #print(machine_ind)
+        #print([1-self.win_prob_lst[machine_ind], self.win_prob_lst[machine_ind]])
+        return np.random.choice(a=[0, 1], p=[1-self.win_prob_lst[machine_ind], self.win_prob_lst[machine_ind]])
 
-plt.ylim(C.min()*1.1,C.max()*1.1)
-plt.yticks([-1, 0, +1],
-[r'$-1$', r'$0$', r'$+1$'])
+    def choose_machine_for_exploration(self):
+        machine_drop_ind = self.means.index(max(self.means))
+        ind_list = [0,1,2]
+        ind_list.remove(machine_drop_ind)
+        return np.random.choice(ind_list)
+        #res = self.play_machine(self, machine_ind)
 
-plt.show()
+    def PlayOneRound(self, itter):
+            itter+=1
+            #choose which machine to play
+            decision = self.exploreORexploit()
+            if decision == 'exploit':
+                #play machhine with a max win probability
+                machine_ind = np.argmax(self.means)
+            else:
+                machine_ind = self.choose_machine_for_exploration()
+
+            res = self.play_machine(machine_ind)
+            #reevaluate mean probability
+            self.means[machine_ind] = (itter-1)*self.means[machine_ind]/itter + res/itter
+
+    def train_agent(self, nitter):
+        self.means = [0, 0, 0]
+        #print(self.means)
+        #print('initial mean: {}'.format(str(self.means)))
+        for itter in range(nitter):
+            #print('itteration {}'.format(str(itter)))
+            self.PlayOneRound(itter)
+
+
+
