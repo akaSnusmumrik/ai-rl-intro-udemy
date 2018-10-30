a73b421747042a9e6288d9a48f137f36706bee38
akaSnusmumrik <samsonov.vl.vl@gmail.com>
2018-10-30 23:12:49 +0100

    getting there

--- TicTacToe.py
+++ TicTacToe.py
@@ -3,7 +3,7 @@ import  numpy as np
 import hashlib
 
 class TicTacToes_Agent:
-    def __init__(self, epsilon, symbol, curiosity):
+    def __init__(self, epsilon, alpha, symbol, curiosity):
         self.field = pd.DataFrame([[0, 0, 0],
                                    [0, 0, 0],
                                    [0, 0, 0]
@@ -11,17 +11,17 @@ class TicTacToes_Agent:
                                   )
         self.value_funct = dict()
         self.episode_states = []
-        self.epsilon = epsilon
+        self.epsilon = epsilon #exploration/exploitation
         self.symbol = symbol
-        self.curiosity = curiosity # [0:1], the higher the more curious the algorithm to unseen states
+        self.curiosity = curiosity # default value function, [0:1], the higher the more curious the algorithm to unseen state
+        self.alpha = alpha #learning rate for value function
 
-
-    def conduct_action(self):
+    def chose_conduct_action(self):
         possible_moves = []
         for i in range(self.field.shape[0]):
             for j in range(self.field.shape[1]):
                 if self.field.iloc[i, j] == 0:
-                    possible_moves.extend([i, j])
+                    possible_moves.append([i, j])
 
         if np.random.choice(10)/10<self.epsilon: #explore
             #pick a random move
@@ -38,18 +38,84 @@ class TicTacToes_Agent:
                 if V > max_V:
                     max_V = V
                     chosen_move = move
-                self.field[move[0], move[1]] = cur_val
+                    self.field[move[0], move[1]] = cur_val
 
         #conduct choosen action:
-        self.field[chosen_move[0], chosen_move[1]] = self.symbol
-        self.episode_states.extend(
+        self.field.iloc[chosen_move[0], chosen_move[1]] = self.symbol
+        self.episode_states.append(
             hashlib.sha256(self.field.values.tobytes()).hexdigest()
             )
 
 
+    def update_value_function(self, winer):
+        if winer == self.symbol:
+            result = 1
+        if winer == 0.5:
+            result = 0.5
+        else:
+            result = -1
+
+        self.value_funct[self.episode_states[-1]] = result #set value function to the last state
+        previous_state = self.episode_states[-1]
+        for state_key in reversed(self.episode_states[:-1]):
+            self.value_funct[state_key] = self.value_funct.get(state_key, self.curiosity) + self.epsilon()*(self.value_funct[previous_state] -
+                                                                                self.value_funct.get(state_key, self.curiosity))
+            previous_state = state_key
+
+class Play:
+    def __init__(self, epsilon, alpha, curiosity, train_episodes):
+        self.field = pd.DataFrame([[0, 0, 0],
+                                   [0, 0, 0],
+                                   [0, 0, 0]
+                                   ]
+                                  )
+        self.agend1 = TicTacToes_Agent(epsilon, alpha, 1, curiosity)
+        self.agend2 = TicTacToes_Agent(epsilon, alpha, -1, curiosity)
+        self.train_episodes = train_episodes
+        self.winer = np.nan
+
+    def gameover_check(self):
+        if ((self.field.sum(axis=1)==3).any())|(
+                (self.field.sum(axis=0) == 3).any())|(
+                sum([self.field.iloc[i, self.field.shape[0] - i - 1] for i in range(self.field.shape[0])]) == 3)|(
+                sum([self.field.iloc[i, i] for i in range(self.field.shape[0])]) == 3):
+            self.winer = 1
+
+        if ((self.field.sum(axis=1) == -3).any())|(
+                (self.field.sum(axis=0) == -3).any())|(
+                sum([self.field.iloc[i, self.field.shape[0] - i - 1] for i in range(self.field.shape[0])]) == -3)|(
+                sum([self.field.iloc[i, i] for i in range(self.field.shape[0])]) == -3):
+            self.winer = -1
+
+        if (self.field == 0).any().any():
+            self.winer=0.5
+
+
+
+
+    def train(self):
+        # training
+        for i in range(self.train_episodes):
+            print("Episode {}".format(i))
+            while np.isnan(self.winer):
+                self.agend1.chose_conduct_action()
+                self.agend2.field = self.agend1.field
+                self.gameover_check()
+                if np.isnan(self.winer)==False:
+                    break
+
+                self.agend2.chose_conduct_action()
+                self.agend1.field = self.agend2.field
+                self.gameover_check()
+                if np.isnan(self.winer)==False:
+                    break
+
+            self.agend1.update_value_function(self.winer)
+            self.agend2.update_value_function(self.winer)
 
 
 
 
 
-
+game = Play(0.7, 1, 0.6, 100)
+game.train()
