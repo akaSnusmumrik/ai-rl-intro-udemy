82ec3a447f578632e997d63946a1e2fcb195fc42
akaSnusmumrik <samsonov.vl.vl@gmail.com>
2018-11-01 00:01:22 +0100

    getting there

--- TicTacToe.py
+++ TicTacToe.py
@@ -31,20 +31,23 @@ class TicTacToes_Agent:
             max_V = 0
             chosen_move = possible_moves[0]
             for move in possible_moves:
-                cur_val = self.field[move[0], move[1]]
-                self.field[move[0], move[1]] = self.symbol
+                cur_val = self.field.iloc[move[0], move[1]]
+                self.field.iloc[move[0], move[1]] = self.symbol
                 state_key = hashlib.sha256(self.field.values.tobytes()).hexdigest()
                 V = self.value_funct.get(state_key, self.curiosity)
                 if V > max_V:
                     max_V = V
                     chosen_move = move
-                    self.field[move[0], move[1]] = cur_val
+                self.field.iloc[move[0], move[1]] = cur_val
 
         #conduct choosen action:
         self.field.iloc[chosen_move[0], chosen_move[1]] = self.symbol
         self.episode_states.append(
             hashlib.sha256(self.field.values.tobytes()).hexdigest()
             )
+        print("_________________")
+        print(self.field)
+
 
 
     def update_value_function(self, winer):
@@ -57,37 +60,40 @@ class TicTacToes_Agent:
 
         self.value_funct[self.episode_states[-1]] = result #set value function to the last state
         previous_state = self.episode_states[-1]
+        self.value_funct[previous_state] = result
         for state_key in reversed(self.episode_states[:-1]):
-            self.value_funct[state_key] = self.value_funct.get(state_key, self.curiosity) + self.epsilon()*(self.value_funct[previous_state] -
+            self.value_funct[state_key] = self.value_funct.get(state_key, self.curiosity) + \
+                                          self.epsilon*(self.value_funct[previous_state] -
                                                                                 self.value_funct.get(state_key, self.curiosity))
             previous_state = state_key
+        self.episode_states = []
 
 class Play:
     def __init__(self, epsilon, alpha, curiosity, train_episodes):
-        self.field = pd.DataFrame([[0, 0, 0],
-                                   [0, 0, 0],
-                                   [0, 0, 0]
-                                   ]
-                                  )
-        self.agend1 = TicTacToes_Agent(epsilon, alpha, 1, curiosity)
-        self.agend2 = TicTacToes_Agent(epsilon, alpha, -1, curiosity)
+        #self.agent1.field = pd.DataFrame([[0, 0, 0],
+        #                           [0, 0, 0],
+        #                           [0, 0, 0]
+        #                           ]
+        #                          )
+        self.agent1 = TicTacToes_Agent(epsilon, alpha, 1, curiosity)
+        self.agent2 = TicTacToes_Agent(epsilon, alpha, -1, curiosity)
         self.train_episodes = train_episodes
         self.winer = np.nan
 
     def gameover_check(self):
-        if ((self.field.sum(axis=1)==3).any())|(
-                (self.field.sum(axis=0) == 3).any())|(
-                sum([self.field.iloc[i, self.field.shape[0] - i - 1] for i in range(self.field.shape[0])]) == 3)|(
-                sum([self.field.iloc[i, i] for i in range(self.field.shape[0])]) == 3):
+        if ((self.agent1.field.sum(axis=1)==3).any())|(
+                (self.agent1.field.sum(axis=0) == 3).any())|(
+                sum([self.agent1.field.iloc[i, self.agent1.field.shape[0] - i - 1] for i in range(self.agent1.field.shape[0])]) == 3)|(
+                sum([self.agent1.field.iloc[i, i] for i in range(self.agent1.field.shape[0])]) == 3):
             self.winer = 1
 
-        if ((self.field.sum(axis=1) == -3).any())|(
-                (self.field.sum(axis=0) == -3).any())|(
-                sum([self.field.iloc[i, self.field.shape[0] - i - 1] for i in range(self.field.shape[0])]) == -3)|(
-                sum([self.field.iloc[i, i] for i in range(self.field.shape[0])]) == -3):
+        if ((self.agent1.field.sum(axis=1) == -3).any())|(
+                (self.agent1.field.sum(axis=0) == -3).any())|(
+                sum([self.agent1.field.iloc[i, self.agent1.field.shape[0] - i - 1] for i in range(self.agent1.field.shape[0])]) == -3)|(
+                sum([self.agent1.field.iloc[i, i] for i in range(self.agent1.field.shape[0])]) == -3):
             self.winer = -1
 
-        if (self.field == 0).any().any():
+        if (self.agent1.field != 0).all().all():
             self.winer=0.5
 
 
@@ -98,20 +104,20 @@ class Play:
         for i in range(self.train_episodes):
             print("Episode {}".format(i))
             while np.isnan(self.winer):
-                self.agend1.chose_conduct_action()
-                self.agend2.field = self.agend1.field
+                if np.isnan(self.winer)==False:
+                    break
+                self.agent1.chose_conduct_action()
+                self.agent2.field = self.agent1.field
                 self.gameover_check()
                 if np.isnan(self.winer)==False:
                     break
 
-                self.agend2.chose_conduct_action()
-                self.agend1.field = self.agend2.field
+                self.agent2.chose_conduct_action()
+                self.agent1.field = self.agent2.field
                 self.gameover_check()
-                if np.isnan(self.winer)==False:
-                    break
 
-            self.agend1.update_value_function(self.winer)
-            self.agend2.update_value_function(self.winer)
+            self.agent1.update_value_function(self.winer)
+            self.agent2.update_value_function(self.winer)
 
 
 
